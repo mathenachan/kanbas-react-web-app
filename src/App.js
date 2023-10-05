935e2284df0acccaeb44ac00ec995788676a743c
mathenachan <66972051+mathenachan@users.noreply.github.com>
2023-10-05 15:57:59 -0400

    fixed app running bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './index.css';
+import './App.css';
+
 function App() {
-   return (
-       <h1>Hello World!</h1>
-   );
+  return (
+      <h1>Hello World!</h1>
+  );
 }
 export default App;
