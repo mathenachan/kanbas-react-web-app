d8e055d311199a3168642eb0ffd4069d184e136f
mathenachan <66972051+mathenachan@users.noreply.github.com>
2023-10-13 04:01:55 -0400

    finished working with arrays 2.2.7 of lab

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,7 +1,13 @@
 import JavaScript from "./JavaScript";
 
 function Assignment3() {
-    return (
+  console.log('Working with Arrays');
+  console.log('numberArray1 = 12345');
+  console.log('stringArray1 = string1string2');
+  console.log('variableArray1 = 25-312345string1string2');
+
+
+  return (
       <div className="container">
         <h1>Assignment 3</h1>
         <JavaScript/>
