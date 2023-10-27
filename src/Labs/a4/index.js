378c62538bba572efa310523ab1bfaf4d12e878e
mathenachan <66972051+mathenachan@users.noreply.github.com>
2023-10-27 19:43:25 -0400

    installed redux

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import ReduxExamples from "./ReduxExamples";
 import Add from "./Add";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
 import ClickEvent from "./ClickEvent";
 import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariable";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
 
 const Assignment4 = () => {
     function sayHello() {
@@ -12,11 +19,18 @@ const Assignment4 = () => {
     return (
         <div className="container">
             <h1>Assignment 4</h1>
+            <ReduxExamples/>
             <Add a={1} b={2} />
             <PassingDataOnEvent/>
             <PassingFunctions theFunction={sayHello} />
             <ClickEvent/>
             <EventObject/>
+            <Counter/>
+            <BooleanStateVariables/>
+            <StringStateVariables/>
+            <DateStateVariable/>
+            <ObjectStateVariable/>
+            <ArrayStateVariable/>
         </div>
     );
 };
