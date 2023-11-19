e3eb40edfae4d3345883a95499696b7cea23d9cd
mathenachan <66972051+mathenachan@users.noreply.github.com>
2023-11-19 18:24:47 -0500

    completed lab

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -11,6 +11,23 @@ function WorkingWithArrays() {
 
     const API = "http://localhost:4000/a5/todos";
     const [todos, setTodos] = useState([]);
+    const updateTodo = async () => {
+        const response = await axios.put(
+            `${API}/${todo.id}`, todo);
+        setTodos(todos.map((t) => (
+            t.id === todo.id ? todo : t)));
+        setTodo({});
+    };
+
+    const postTodo = async () => {
+        const response = await axios.post(API, todo);
+        setTodos([...todos, response.data]);
+    };
+    const deleteTodo = async (todo) => {
+        const response = await axios.delete(`${API}/${todo.id}`);
+        setTodos(todos.filter((t) => t.id !== todo.id));
+    };
+
     const fetchTodos = async () => {
         const response = await axios.get(API);
         setTodos(response.data);
@@ -59,6 +76,34 @@ function WorkingWithArrays() {
                 className="form-control mb-2"
                 type="text"
             />
+            <textarea className="form-control"
+                onChange={(e) => setTodo({
+                    ...todo,
+                    description: e.target.value
+                })}
+                value={todo.description} type="text"
+            />
+            <input className="form-control"
+                onChange={(e) => setTodo({
+                    ...todo, due: e.target.value
+                })}
+                value={todo.due} type="date"
+            />
+            <label>
+                <input
+                    onChange={(e) => setTodo({
+                        ...todo, completed: e.target.checked
+                    })}
+                    value={todo.completed} type="checkbox"
+                />
+                Completed
+            </label>
+            <button className="btn btn-warning mb-2 w-100" onClick={postTodo} >
+                Post Todo
+            </button>
+            <button onClick={updateTodo}>
+                Update Todo
+            </button>
             <button onClick={createTodo}
                 className="btn btn-primary mb-2 w-100">
                 Create Todo
@@ -67,6 +112,7 @@ function WorkingWithArrays() {
                 className="btn btn-success mb-2 w-100">
                 Update Title
             </button>
+
 
 
             <ul className="list-group">
@@ -80,11 +126,19 @@ function WorkingWithArrays() {
                         </button>
 
                         <button
-                            onClick={() => removeTodo(todo)}
-                            className="btn btn-danger float-end" >
-                            Remove
+                            onClick={() => deleteTodo(todo)}
+                            className="btn btn-danger float-end ms-2">
+                            Delete
                         </button>
+
+                        <input
+                            checked={todo.completed}
+                            type="checkbox" readOnly
+                        />
                         {todo.title}
+                        <p>{todo.description}</p>
+                        <p>{todo.due}</p>
+
                     </li>
                 ))}
             </ul>
