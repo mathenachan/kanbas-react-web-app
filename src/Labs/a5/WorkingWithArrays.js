b41ef3a5c68b7da7be3b963e45d02d7728374198
mathenachan <66972051+mathenachan@users.noreply.github.com>
2023-11-20 19:52:30 -0500

    refactored to use variable

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -9,7 +9,7 @@ function WorkingWithArrays() {
         completed: false,
     });
 
-    const API = "http://localhost:4000/a5/todos";
+    const API = process.env.REACT_APP_BASE`/a5/todos`;
     const [todos, setTodos] = useState([]);
     const updateTodo = async () => {
         const response = await axios.put(
